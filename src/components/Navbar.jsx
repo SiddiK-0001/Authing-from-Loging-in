1a788712fa641e0695d13257ba0ff77ba9966e55
SiddiK-0001 <absiddik109816@gmail.com>
2024-11-16 18:23:05 +0600

    starting from not so scratch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,18 @@ import userIcon from "../assets/user.png";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
+
       <div className=""></div>
+
       <div className="nav space-x-5">
         <Link to="/">Home</Link>
         <Link to="/career">Career</Link>
         <Link to="/about">About</Link>
       </div>
-      <div className="login flex gap-2 items-center">
-        <div className=" ">
-          <img src={userIcon} alt="" />
-        </div>
-        <button className="btn btn-neutral rounded-none">Login</button>
+
+      <div className="flex gap-2  items-center">
+        <img className="h-8"  src={userIcon} alt="" />
+        <button className="btn bg-black h-5 btn-sm rounded-none text-white">Login</button>
       </div>
     </div>
   );
